a43e01eb1a462f0e6512126ec5ae82ad72a0b5c4
naycoma <170476470+naycoma@users.noreply.github.com>
2026-04-03 22:17:34 +0900

    :fire: Clean up Compat.cs by removing unused methods

--- 1.5/Source/Compat.cs
+++ 1.5/Source/Compat.cs
@@ -1,16 +1,8 @@
 using UnityEngine;
 
-using RimWorld;
-
 namespace GoodwillPreview;
 
 internal static class Compat {
-    public static float GetMaxFuelLevel(this CompLaunchable launcher) =>
-        launcher.FuelingPortSource?.GetComp<CompRefuelable>()?.Props.fuelCapacity ?? 0f;
-
-    public static int MaxLaunchDistanceAtFuelLevel(this CompLaunchable _, float fuelLevel) =>
-        CompLaunchable.MaxLaunchDistanceAtFuelLevel(fuelLevel);
-
     public static Rect MiddlePartPixels(this Rect rect, float width, float height) =>
         new(rect.center.x - width / 2f, rect.center.y - height / 2f, width, height);
 }
--- 1.6/Source/Compat.cs
+++ 1.6/Source/Compat.cs
@@ -1,8 +1,5 @@
-using RimWorld;
-
 namespace GoodwillPreview;
 
 internal static class Compat {
-    public static float GetMaxFuelLevel(this CompLaunchable launcher) =>
-        launcher.MaxFuelLevel;
+
 }
