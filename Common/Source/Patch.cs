eb26cb153eb99349600c618266d93fd5aeec4253
naycoma <170476470+naycoma@users.noreply.github.com>
2026-04-04 15:43:42 +0900

    :recycle: logging

--- Common/Source/Patch.cs
+++ Common/Source/Patch.cs
@@ -10,26 +10,32 @@ namespace GoodwillPreview;
 
 [HarmonyPatch]
 public static class Patch {
+    public const bool DEBUG =
+#if DEBUG
+    true;
+# else
+    false;
+# endif
+
     [HarmonyPatch(typeof(Dialog_LoadTransporters))]
     [HarmonyPatch(nameof(Dialog_LoadTransporters.PostOpen)), HarmonyPostfix]
     public static void PostOpen_Postfix(Dialog_LoadTransporters __instance) {
-        // CompLaunchable launcher = __instance.transporters[0].Launchable;
-        // int distance = launcher.MaxLaunchDistanceAtFuelLevel(launcher.GetMaxFuelLevel());
+        if (DEBUG) Log.Message($"[{nameof(Dialog_LoadTransporters.PostOpen)}][Postfix] Dialog opened");
         int tile = __instance.map.Tile;
-        // Log.Message($"Tile: {tile}, Radius: {distance}");
-        // Log.Message($"Transporters: {__instance.transporters.Count}");
         Mod.ReloadSettlements(SettlementUtility.GetSettlementsWithinRadius(tile));
     }
 
     [HarmonyPatch(typeof(Dialog_LoadTransporters))]
     [HarmonyPatch(nameof(Dialog_LoadTransporters.DoWindowContents)), HarmonyPrefix]
     public static void DoWindowContents_Prefix(Dialog_LoadTransporters __instance) {
+        if (DEBUG) Log.Message($"[{nameof(Dialog_LoadTransporters.DoWindowContents)}][Prefix] Window contents initialized");
         Mod.Open(__instance);
     }
 
     [HarmonyPatch(typeof(TransferableUIUtility))]
     [HarmonyPatch(nameof(TransferableUIUtility.DrawExtraInfo)), HarmonyPrefix]
     public static void DrawExtraInfo_Prefix(ref List<TransferableUIUtility.ExtraInfo> info, ref Rect rect) {
+        if (DEBUG) Log.Message($"[{nameof(TransferableUIUtility.DrawExtraInfo)}][Prefix] Initial info count: {info.Count}");
         if (Mod.Empty()) return;
         bool showFloatMenu = Find.WindowStack.Windows.Any(w => w is FloatMenu);
         if (Mod.ExtraInfo(includeTip: !showFloatMenu) is { } e)
@@ -39,6 +45,7 @@ public static class Patch {
     [HarmonyPatch(typeof(TransferableUIUtility))]
     [HarmonyPatch(nameof(TransferableUIUtility.DrawExtraInfo)), HarmonyPostfix]
     public static void DrawExtraInfo_Postfix(List<TransferableUIUtility.ExtraInfo> info, Rect rect) {
+        if (DEBUG) Log.Message($"[{nameof(TransferableUIUtility.DrawExtraInfo)}][Postfix] Info count: {info.Count}");
         if (Mod.Empty()) return;
         float maxWidth = info.Count * 230f;
         if (rect.width > maxWidth)
@@ -50,9 +57,9 @@ public static class Patch {
         int markedIndex = info.FindIndex(Mod.IsMarkedExtraInfo);
         if (markedIndex >= 0) {
             Faction displayed = Mod.CurrentSettlement().Faction;
-            
+
             Rect infoRect = rect.RightPart(1f - (float)markedIndex / info.Count);
-            
+
             if (Mod.ButtonGoodwillExtraInfo(infoRect, displayed))
                 Find.WindowStack.Add(Mod.CreateDropdownMenu());
         }
@@ -63,12 +70,14 @@ public static class Patch {
     [HarmonyPatch(typeof(Dialog_LoadTransporters))]
     [HarmonyPatch(nameof(Dialog_LoadTransporters.CountToTransferChanged)), HarmonyPostfix]
     public static void CountToTransferChanged_Postfix(Dialog_LoadTransporters __instance) {
+        if (DEBUG) Log.Message($"[{nameof(Dialog_LoadTransporters.CountToTransferChanged)}][Postfix] Count changed");
         Mod.ChangedCount();
     }
 
     [HarmonyPatch(typeof(Dialog_LoadTransporters))]
     [HarmonyPatch(nameof(Dialog_LoadTransporters.DoWindowContents)), HarmonyPostfix]
     public static void DoWindowContents_Postfix(Dialog_LoadTransporters __instance) {
+        if (DEBUG) Log.Message($"[{nameof(Dialog_LoadTransporters.DoWindowContents)}][Postfix] Window contents drawn");
         Mod.Close();
     }
 }
